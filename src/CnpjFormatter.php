be8fa0318f1d2b527556cf556a7e4efc892c4fbf
juliolmuller <juliolmuller@outlook.com>
2025-09-09 01:23:20 -0300

    refactor: redefine namespaces

--- src/CnpjFormatter.php
+++ src/CnpjFormatter.php
@@ -2,16 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Lacus\Cnpj\Fmt;
+namespace Lacus\Formatters\Cnpj;
 
-/**
- * Formatador de CNPJ brasileiro
- */
 class CnpjFormatter
 {
-    /**
-     * Formata um CNPJ com máscara (XX.XXX.XXX/XXXX-XX)
-     */
     public function format(string $cnpj): string
     {
         $cnpj = $this->clean($cnpj);
@@ -25,21 +19,5 @@ class CnpjFormatter
                substr($cnpj, 5, 3) . '/' .
                substr($cnpj, 8, 4) . '-' .
                substr($cnpj, 12, 2);
-    }
-
-    /**
-     * Remove formatação de um CNPJ
-     */
-    public function clean(string $cnpj): string
-    {
-        return preg_replace('/[^0-9]/', '', $cnpj);
-    }
-
-    /**
-     * Verifica se um CNPJ está formatado
-     */
-    public function isFormatted(string $cnpj): bool
-    {
-        return preg_match('/^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/', $cnpj) === 1;
     }
 }
