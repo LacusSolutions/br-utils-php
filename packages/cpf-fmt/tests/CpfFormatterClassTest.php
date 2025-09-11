d11609f4df35d4e49ac379342c68995da9e40b23
juliolmuller <juliolmuller@outlook.com>
2025-09-11 01:08:43 -0300

    test(cpf-fmt): implement unit tests

---
+++ packages/cpf-fmt/tests/CpfFormatterClassTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CpfFmt\Tests;
+
+use Lacus\CpfFmt\CpfFormatter;
+
+class CpfFormatterClassTest extends CpfFormatterTestCase
+{
+    private CpfFormatter $formatter;
+
+    protected function setUp(): void
+    {
+        $this->formatter = new CpfFormatter();
+    }
+
+    protected function format(
+        string $cpfString,
+        ?bool $escape = null,
+        ?bool $hidden = null,
+        ?string $hiddenKey = null,
+        ?int $hiddenStart = null,
+        ?int $hiddenEnd = null,
+        ?string $dotKey = null,
+        ?string $dashKey = null,
+        ?callable $onFail = null,
+    ): string {
+        return $this->formatter->format(
+            $cpfString,
+            $escape,
+            $hidden,
+            $hiddenKey,
+            $hiddenStart,
+            $hiddenEnd,
+            $dotKey,
+            $dashKey,
+            $onFail,
+        );
+    }
+
+    public function testObjectOrientedGetOptions(): void
+    {
+        $options = $this->formatter->getOptions();
+
+        $this->assertInstanceOf(\Lacus\CpfFmt\CpfFormatterOptions::class, $options);
+        $this->assertFalse($options->isEscaped());
+        $this->assertFalse($options->isHidden());
+        $this->assertEquals('*', $options->getHiddenKey());
+        $this->assertEquals(3, $options->getHiddenStart());
+        $this->assertEquals(10, $options->getHiddenEnd());
+        $this->assertEquals('.', $options->getDotKey());
+        $this->assertEquals('-', $options->getDashKey());
+    }
+}
---
+++ packages/cpf-fmt/tests/CpfFormatterFunctionTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CpfFmt\Tests;
+
+use function Lacus\CpfFmt\cpf_fmt;
+
+class CpfFormatterFunctionTest extends CpfFormatterTestCase
+{
+    protected function format(
+        string $cpfString,
+        ?bool $escape = null,
+        ?bool $hidden = null,
+        ?string $hiddenKey = null,
+        ?int $hiddenStart = null,
+        ?int $hiddenEnd = null,
+        ?string $dotKey = null,
+        ?string $dashKey = null,
+        ?callable $onFail = null,
+    ): string {
+        return cpf_fmt(
+            $cpfString,
+            $escape,
+            $hidden,
+            $hiddenKey,
+            $hiddenStart,
+            $hiddenEnd,
+            $dotKey,
+            $dashKey,
+            $onFail,
+        );
+    }
+}
