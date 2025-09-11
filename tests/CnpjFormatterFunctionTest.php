65aa7f0b3976176155d745dd9117e6065c5037c2
juliolmuller <juliolmuller@outlook.com>
2025-09-11 16:18:09 -0300

    test(cnpj-fmt): create tests for source code

---
+++ tests/CnpjFormatterFunctionTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CnpjFmt\Tests;
+
+use function Lacus\CnpjFmt\cnpj_fmt;
+
+class CnpjFormatterFunctionTest extends CnpjFormatterTestCase
+{
+    protected function format(
+        string $cnpjString,
+        ?bool $escape = null,
+        ?bool $hidden = null,
+        ?string $hiddenKey = null,
+        ?int $hiddenStart = null,
+        ?int $hiddenEnd = null,
+        ?string $dotKey = null,
+        ?string $slashKey = null,
+        ?string $dashKey = null,
+        ?callable $onFail = null,
+    ): string {
+        return cnpj_fmt(
+            $cnpjString,
+            $escape,
+            $hidden,
+            $hiddenKey,
+            $hiddenStart,
+            $hiddenEnd,
+            $dotKey,
+            $slashKey,
+            $dashKey,
+            $onFail,
+        );
+    }
+}
