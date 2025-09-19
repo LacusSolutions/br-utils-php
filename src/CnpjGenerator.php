6612a6088dbf2ac1ca88b0b76d61d54f7a672b2e
juliolmuller <juliolmuller@outlook.com>
2025-09-19 15:57:23 -0300

    feat(cnpj-gen): implement generator to CNPJ

--- src/CnpjGenerator.php
+++ src/CnpjGenerator.php
@@ -6,12 +6,14 @@ namespace Lacus\CnpjGen;
 
 class CnpjGenerator
 {
+    private CnpjGeneratorVerifierDigit $verifierDigit;
     private CnpjGeneratorOptions $options;
 
     public function __construct(
         ?bool $format = null,
         ?string $prefix = null,
     ) {
+        $this->verifierDigit = new CnpjGeneratorVerifierDigit();
         $this->options = new CnpjGeneratorOptions(
             $format,
             $prefix,
@@ -27,14 +29,64 @@ class CnpjGenerator
             $prefix,
         );
 
-        // TODO: Implement the logic to generate a valid CNPJ
-        $generatedCnpj = '';
+        $prefixArray = str_split($actualOptions->getPrefix());
+        $prefixNumbers = array_map('intval', $prefixArray);
 
-        return $generatedCnpj;
+        $businessId = $this->generateBusinessId($prefixNumbers);
+        $branchId = $this->generateBranchId($prefixNumbers);
+        $cnpjSequence = array_merge($businessId, $branchId);
+        $cnpjSequence[] = $this->verifierDigit->calculate($cnpjSequence);
+        $cnpjSequence[] = $this->verifierDigit->calculate($cnpjSequence);
+        $cnpjGenerated = implode('', $cnpjSequence);
+
+        if ($actualOptions->isFormatting()) {
+            return $this->format($cnpjGenerated);
+        }
+
+        return $cnpjGenerated;
     }
 
     public function getOptions(): CnpjGeneratorOptions
     {
         return $this->options;
+    }
+
+    private function generateBusinessId(array $prefixNumbers): array
+    {
+        $businessIdLength = 8;
+
+        $businessIdStart = array_slice($prefixNumbers, 0, $businessIdLength);
+        $businessIdStartLength = count($businessIdStart);
+
+        $businessIdEnd = array_fill(0, $businessIdLength - $businessIdStartLength, null);
+        $businessIdEnd = array_map(fn () => rand(0, 9), $businessIdEnd);
+
+        return array_merge($businessIdStart, $businessIdEnd);
+    }
+
+    private function generateBranchId(array $prefixNumbers): array
+    {
+        $branchIdLength = 4;
+
+        $branchIdStart = array_slice($prefixNumbers, 8, $branchIdLength);
+        $branchIdStartLength = count($branchIdStart);
+
+        $branchIdEnd = array_fill(0, $branchIdLength - $branchIdStartLength, 0);
+        $branchIdEndLength = count($branchIdEnd);
+
+        if ($branchIdEndLength > 0) {
+            $branchIdEnd[$branchIdEndLength - 1] = rand(1, 9);
+        }
+
+        return array_merge($branchIdStart, $branchIdEnd);
+    }
+
+    private function format(string $cnpjString): string
+    {
+        return substr($cnpjString, 0, 2) . '.' .
+               substr($cnpjString, 2, 3) . '.' .
+               substr($cnpjString, 5, 3) . '/' .
+               substr($cnpjString, 8, 4) . '-' .
+               substr($cnpjString, 12, 2);
     }
 }
