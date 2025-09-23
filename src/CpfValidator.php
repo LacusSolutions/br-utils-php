6f51f9b6e363fb273c865183ee104f987a27b87d
juliolmuller <juliolmuller@outlook.com>
2025-09-23 01:36:30 -0300

    feat(cpf-val): implement source code

--- src/CpfValidator.php
+++ src/CpfValidator.php
@@ -4,11 +4,45 @@ declare(strict_types=1);
 
 namespace Lacus\CpfVal;
 
+use Lacus\CpfGen\CpfGeneratorVerifierDigit;
+
 class CpfValidator
 {
+    private CpfGeneratorVerifierDigit $verifierDigit;
+
+    public function __construct()
+    {
+        $this->verifierDigit = new CpfGeneratorVerifierDigit();
+    }
+
     public function isValid(string $cpfString): bool
     {
-        // TODO: Implement the logic to isValid a valid CPF
-        return $cpfString !== '';
+        $cpfNumbersString = preg_replace('/[^0-9]/', '', $cpfString);
+        $cpfNumbersStringArray = str_split($cpfNumbersString);
+        $cpfNumbersArray = array_map('intval', $cpfNumbersStringArray);
+
+        if (count($cpfNumbersArray) !== CPF_LENGTH) {
+            return false;
+        }
+
+        $firstVerifierDigitIndex = CPF_LENGTH - 2;
+        $providedFirstVerifierDigit = $cpfNumbersArray[$firstVerifierDigitIndex];
+        $baseFirstVerifierDigitCalculation = array_slice($cpfNumbersArray, 0, $firstVerifierDigitIndex);
+        $calculatedFirstVerifierDigit = $this->verifierDigit->calculate($baseFirstVerifierDigitCalculation);
+
+        if ($providedFirstVerifierDigit !== $calculatedFirstVerifierDigit) {
+            return false;
+        }
+
+        $secondVerifierDigitIndex = CPF_LENGTH - 1;
+        $baseSecondVerifierDigitCalculation = array_slice($cpfNumbersArray, 0, $secondVerifierDigitIndex);
+        $providedSecondVerifierDigit = $cpfNumbersArray[$secondVerifierDigitIndex];
+        $calculatedSecondVerifierDigit = $this->verifierDigit->calculate($baseSecondVerifierDigitCalculation);
+
+        if ($providedSecondVerifierDigit !== $calculatedSecondVerifierDigit) {
+            return false;
+        }
+
+        return true;
     }
 }
--- src/cpf-val.php
+++ src/cpf-val.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Lacus\CpfVal;
 
+const CPF_LENGTH = 11;
+
 function cpf_val(string $cpfString): bool
 {
     $formatter = new CpfValidator();
