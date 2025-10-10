53f16a3c321cb1d38038685afc8010f4f8335f7d
juliolmuller <juliolmuller@outlook.com>
2025-10-10 12:16:39 -0300

    feat(cpf-utils): create package to consolidate CPF resources

---
+++ packages/cpf-utils/src/CpfFormatter.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CpfUtils;
+
+use Lacus\CpfFmt\CpfFormatter as BaseCpfFormatter;
+
+class CpfFormatter extends BaseCpfFormatter
+{
+}
---
+++ packages/cpf-utils/src/CpfValidator.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CpfUtils;
+
+use Lacus\CpfVal\CpfValidator as BaseCpfValidator;
+
+class CpfValidator extends BaseCpfValidator
+{
+}
