dea419dd6f8821ffe7dbd05234c835d4030fe40e
juliolmuller <juliolmuller@outlook.com>
2025-10-10 12:11:40 -0300

    feat(cnpj-utils): create package to consolidate CNPJ resources

---
+++ src/CnpjFormatter.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CnpjUtils;
+
+use Lacus\CnpjFmt\CnpjFormatter as BaseCnpjFormatter;
+
+class CnpjFormatter extends BaseCnpjFormatter
+{
+}
---
+++ src/cnpj_utils.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\CnpjUtils;
+
+function cnpj_fmt(
+    string $cnpjString,
+    ?bool $escape = null,
+    ?bool $hidden = null,
+    ?string $hiddenKey = null,
+    ?int $hiddenStart = null,
+    ?int $hiddenEnd = null,
+    ?string $dotKey = null,
+    ?string $slashKey = null,
+    ?string $dashKey = null,
+    ?callable $onFail = null,
+): string {
+    return \Lacus\CnpjFmt\cnpj_fmt(
+        $cnpjString,
+        $escape,
+        $hidden,
+        $hiddenKey,
+        $hiddenStart,
+        $hiddenEnd,
+        $dotKey,
+        $slashKey,
+        $dashKey,
+        $onFail,
+    );
+}
+
+function cnpj_gen(
+    ?bool $format = null,
+    ?string $prefix = null,
+): string {
+    return \Lacus\CnpjGen\cnpj_gen(
+        $format,
+        $prefix,
+    );
+}
+
+function cnpj_val(string $cnpjString): bool
+{
+    return \Lacus\CnpjVal\cnpj_val($cnpjString);
+}
