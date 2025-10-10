a65d30dd41644795fb8166bd3cd2490cb20a6cd2
juliolmuller <juliolmuller@outlook.com>
2025-10-10 00:49:10 -0300

    test(cpf-val): convert test cases class into trait for reusability

--- tests/CpfValidatorFunctionTest.php
+++ tests/CpfValidatorFunctionTest.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace Lacus\CpfVal\Tests;
 
+use PHPUnit\Framework\TestCase;
+
 use function Lacus\CpfVal\cpf_val;
 
-class CpfValidatorFunctionTest extends CpfValidatorTestCase
+class CpfValidatorFunctionTest extends TestCase
 {
+    use CpfValidatorTestCases;
+
     protected function isValid(string $cpfString): bool
     {
         return cpf_val($cpfString);
