c37f23614fc27b30e5336dac981803b9be410ec6
juliolmuller <juliolmuller@outlook.com>
2025-10-19 18:40:35 -0300

    feat(br-utils): create main class

---
+++ src/BrUtils.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\BrUtils;
+
+use InvalidArgumentException;
+
+/**
+ * @property-read CpfUtils $cpf
+ * @property-read CnpjUtils $cnpj
+ */
+class BrUtils
+{
+    private CnpjUtils $cnpjUtils;
+    private CpfUtils $cpfUtils;
+
+    /**
+     * @param array{
+     *   formatter?: array{
+     *     escape?: bool,
+     *     hidden?: bool,
+     *     hiddenKey?: string,
+     *     hiddenStart?: int,
+     *     hiddenEnd?: int,
+     *     dotKey?: string,
+     *     slashKey?: string,
+     *     dashKey?: string,
+     *     onFail?: Closure,
+     *   },
+     *   generator?: array{
+     *     format?: bool,
+     *     prefix?: string,
+     *   },
+     * } $cnpj
+     * @param array{
+     *     formatter?: array{
+     *       escape?: bool,
+     *       hidden?: bool,
+     *       hiddenKey?: string,
+     *       hiddenStart?: int,
+     *       hiddenEnd?: int,
+     *       dotKey?: string,
+     *       dashKey?: string,
+     *       onFail?: Closure,
+     *     },
+     *     generator?: array{
+     *       format?: bool,
+     *       prefix?: string,
+     *     },
+     * } $cpf
+     */
+    public function __construct(
+        array $cnpj = [],
+        array $cpf = [],
+    ) {
+        $this->cpfUtils = new CpfUtils(...$cpf);
+        $this->cnpjUtils = new CnpjUtils(...$cnpj);
+    }
+
+    public function __get(string $name): mixed
+    {
+        return match ($name) {
+            'cpf' => $this->cpfUtils,
+            'cnpj' => $this->cnpjUtils,
+            default => throw new InvalidArgumentException("Property {$name} not found"),
+        };
+    }
+
+    public function getCpfUtils(): CpfUtils
+    {
+        return $this->cpfUtils;
+    }
+
+    public function getCnpjUtils(): CnpjUtils
+    {
+        return $this->cnpjUtils;
+    }
+}
