54f6d2d16dbef53edc046a0d9a4a33b97b15a78e
juliolmuller <juliolmuller@outlook.com>
2026-05-08 10:57:11 -0300

    feat(cnpj-gen): implement helper function

--- packages/cnpj-gen/src/cnpj-gen.php
+++ packages/cnpj-gen/src/cnpj-gen.php
@@ -2,18 +2,44 @@
 
 declare(strict_types=1);
 
-namespace Lacus\CnpjGen;
+namespace Lacus\BrUtils\Cnpj;
 
-const CNPJ_LENGTH = 14;
+use Lacus\BrUtils\Cnpj\Enums\CnpjType;
+use Lacus\BrUtils\Cnpj\Exceptions\CnpjGeneratorOptionPrefixInvalidException;
+use Lacus\BrUtils\Cnpj\Exceptions\CnpjGeneratorOptionsTypeError;
+use Lacus\BrUtils\Cnpj\Exceptions\CnpjGeneratorOptionTypeInvalidException;
 
+/**
+ * Helper function to simplify the usage of the {@see CnpjGenerator} class.
+ *
+ * Formats a CNPJ string according to the given options. With no options,
+ * returns the traditional CNPJ format (e.g. `12.345.678/0009-10`). Invalid
+ * input length is handled by the configured `onFail` callback instead of
+ * throwing.
+ *
+ * @param ?CnpjGeneratorOptions $options
+ * @param ?bool $format
+ * @param ?string $prefix
+ * @param ?(CnpjType|'alphanumeric'|'alphabetic'|'numeric') $type
+ *
+ * @throws CnpjGeneratorOptionsTypeError If any option has an invalid type.
+ * @throws CnpjGeneratorOptionPrefixInvalidException If the `prefix` option
+ *   contains an invalid combination of characters.
+ * @throws CnpjGeneratorOptionTypeInvalidException If the `type` option is
+ *   not one of the allowed values.
+ */
 function cnpj_gen(
-    ?bool $format = null,
-    ?string $prefix = null,
+    $options = null,
+    $format = null,
+    $prefix = null,
+    $type = null,
 ): string {
-    $formatter = new CnpjGenerator(
+    $generator = new CnpjGenerator(
+        $options,
         $format,
         $prefix,
+        $type,
     );
 
-    return $formatter->generate();
+    return $generator->generate();
 }
