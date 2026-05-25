5d61fc0f2e71f09ca49366055c24915b6e1018e3
juliolmuller <juliolmuller@outlook.com>
2026-05-25 17:43:01 -0300

    chore(cnpj-utils): migrate test framework from PhpUnit to Pest

---
+++ tests/Pest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+|--------------------------------------------------------------------------
+| Pest bootstrap
+|--------------------------------------------------------------------------
+*/
+
+use PHPUnit\Framework\TestCase;
+
+$specsDirectory = __DIR__ . DIRECTORY_SEPARATOR . 'Specs' . DIRECTORY_SEPARATOR;
+
+uses(TestCase::class)->in($specsDirectory);
+
+uses()->group('isolated-process-tests')->in("{$specsDirectory}*.isolated.spec.php");
+
+uses()->afterEach(fn () => Mockery::close());
