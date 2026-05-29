662c42da74b2eed7351d0e220155f68b37d7c8ac
juliolmuller <juliolmuller@outlook.com>
2026-05-29 14:55:15 -0300

    test(cnpj-utils): implement latest unit tests for `CnpjUtils`

--- packages/cnpj-utils/tests/Pest.php
+++ packages/cnpj-utils/tests/Pest.php
@@ -10,10 +10,6 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-$specsDirectory = __DIR__ . DIRECTORY_SEPARATOR . 'Specs' . DIRECTORY_SEPARATOR;
+$specsDirectory = __DIR__ . DIRECTORY_SEPARATOR . 'specs' . DIRECTORY_SEPARATOR;
 
 uses(TestCase::class)->in($specsDirectory);
-
-uses()->group('isolated-process-tests')->in("{$specsDirectory}*.isolated.spec.php");
-
-uses()->afterEach(fn () => Mockery::close());
