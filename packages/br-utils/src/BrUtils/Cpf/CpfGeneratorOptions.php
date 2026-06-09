9c89d3b430500c483bf800b281b79224d4209d4a
juliolmuller <juliolmuller@outlook.com>
2026-06-09 00:33:50 -0300

    feat(br-utils): add resources to CPF API
    Introduced new classes for CPF formatting, generation, and utility functions, extending existing base classes for enhanced functionality.

---
+++ packages/br-utils/src/BrUtils/Cpf/CpfGeneratorOptions.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lacus\BrUtils\Cpf;
+
+use Lacus\CpfGen\CpfGeneratorOptions as BaseCpfGeneratorOptions;
+
+class CpfGeneratorOptions extends BaseCpfGeneratorOptions
+{
+}
