65c6813022074f2338d78c1f9d69c1c269dd2cc5
juliolmuller <juliolmuller@outlook.com>
2026-06-09 00:30:49 -0300

    feat(br-utils): update resources namespaces
    BREAKING CHANGE: renname namespace of CPF-related resources, to match latest standards.

--- packages/br-utils/src/BrUtils/Cpf/CpfFormatter.php
+++ packages/br-utils/src/BrUtils/Cpf/CpfFormatter.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Lacus\BrUtils\Cpf;
 
-use Lacus\CpfUtils\CpfFormatter as BaseCpfFormatter;
+use Lacus\CpfFmt\CpfFormatter as BaseCpfFormatter;
 
 class CpfFormatter extends BaseCpfFormatter
 {
